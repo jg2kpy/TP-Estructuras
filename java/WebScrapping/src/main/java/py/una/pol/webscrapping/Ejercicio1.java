e5ec11d7d249f15fa58e6056e4b829f304be6c77
Jose Luis Junior Gutierrez Aguero <jlgutierrez2000@hotmail.com>
2022-05-03 17:08:18 -0400

    agregue comentarios

--- java/WebScrapping/src/main/java/py/una/pol/webscrapping/Ejercicio1.java
+++ java/WebScrapping/src/main/java/py/una/pol/webscrapping/Ejercicio1.java
@@ -1,16 +1,25 @@
 package py.una.pol.webscrapping;
 
+// Importamos librerias, librerias de ArrayList
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+
+//Liberias para registrar excepciones
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+//Libreria de Jsoup (analizador de HTML)
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+//Librerias de entrada y salida a sistema de ficheros
 import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
+
+//Libreria para graficar los datos
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.ChartUtils;
 import org.jfree.chart.ChartFactory;
@@ -24,24 +33,25 @@ import java.awt.Desktop;
  */
 public class Ejercicio1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {// Funcion principal
 
+        //ArrayList que contiene la lista de los 20 lenguajes mas usados segun tiobe index
         ArrayList<Lenguaje> tiobe_index = new ArrayList<Lenguaje>(Arrays.asList(new Lenguaje("Python", 0, 0), new Lenguaje("C", 0, 0), new Lenguaje("Java", 0, 0), new Lenguaje("Cpp", 0, 0), new Lenguaje("Csharp", 0, 0), new Lenguaje("VisualBasic", 0, 0), new Lenguaje("JavaScript", 0, 0), new Lenguaje("Assembly language", 0, 0), new Lenguaje("SQL", 0, 0), new Lenguaje("PHP", 0, 0), new Lenguaje("R", 0, 0), new Lenguaje("Delphi", 0, 0), new Lenguaje("Go", 0, 0), new Lenguaje("Swift", 0, 0), new Lenguaje("Ruby", 0, 0), new Lenguaje("visual-basic-6", 0, 0), new Lenguaje("Objective-C", 0, 0), new Lenguaje("Perl", 0, 0), new Lenguaje("Lua", 0, 0), new Lenguaje("matlab", 0, 0)));
 
         int MAX = 0;
         int MIN = 0;
 
-        try {
+        try { // Empezamos el Web Scrapping
             FileWriter salida = new FileWriter("Resultados.txt");
 
-            System.out.println("Realizando WebScrapping a github.com y escribendo resultados en Resultados.txt ...");
+            System.out.println("Realizando WebScrapping a github.com y escribendo resultados en Resultados.txt");
             System.out.println("Esta operacion puede tomar aproximadamente 1 minuto...");
 
-            for (Lenguaje lenguaje : tiobe_index) {
+            for (Lenguaje lenguaje : tiobe_index) {// Iteramos la lista para obtener la cantidad de repositorios de github por cada topico
 
                 int tries = 0;
-                String match_topic = getCantidad(lenguaje.nombre);
-                while ("".equals(match_topic) && tries < 3) {
+                String match_topic = getCantidad(lenguaje.nombre); // Solicitud HTTP a github.com/topics
+                while ("".equals(match_topic) && tries < 3) { // Si no obtenemos la respuesta probamos otra 3 veces, si aun no tenemos respuesta se saca ese lenguaje de la lista
                     Thread.sleep(1000);
                     tries = tries + 1;
                     match_topic = getCantidad(lenguaje.nombre);
@@ -53,7 +63,7 @@ public class Ejercicio1 {
                 } else {
 
                     lenguaje.cantidad = procesarCadena(match_topic);
-                    salida.write(lenguaje.toFile() + "\n");
+                    salida.write(lenguaje.toFile() + "\n"); // Guardamos los resultado en el archivo Resultados.txt
 
                     if (MAX < lenguaje.cantidad) {
                         MAX = lenguaje.cantidad;
@@ -68,11 +78,11 @@ public class Ejercicio1 {
             salida.close();
 
             float dif = MAX - MIN;
-            for (Lenguaje lenguaje : tiobe_index) {
+            for (Lenguaje lenguaje : tiobe_index) {// Aplicamos la formula de GitHub Rating
                 lenguaje.rating = ((float) (lenguaje.cantidad - MIN) / dif) * 100F;
             }
 
-            Collections.sort(tiobe_index, (Lenguaje l1, Lenguaje l2) -> {
+            Collections.sort(tiobe_index, (Lenguaje l1, Lenguaje l2) -> { // Ordenamos la lista
                 return l1.cantidad - l2.cantidad;
             });
 
@@ -83,7 +93,7 @@ public class Ejercicio1 {
             DefaultCategoryDataset dataset = new DefaultCategoryDataset();
 
             int n = tiobe_index.size() - 1;
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 10; i++) {// Graficamos mediante JFreeChart
                 dataset.addValue(tiobe_index.get(n - i).cantidad, tiobe_index.get(n - i).nombre, "");
             }
 
@@ -103,8 +113,8 @@ public class Ejercicio1 {
     public static String getCantidad(String lenguaje) {
         String texto = "";
         try {
-            Document doc = Jsoup.connect("https://github.com/topics/" + lenguaje).get();
-            Elements repositories = doc.getElementsByClass("h3 color-fg-muted");
+            Document doc = Jsoup.connect("https://github.com/topics/" + lenguaje).get(); // Solicitud HTTP a github.com/topics
+            Elements repositories = doc.getElementsByClass("h3 color-fg-muted"); // Usamos el atributo HTML class para obtener la informacion que queremos
             texto = repositories.text();
         } catch (IOException ex) {
             //Logger.getLogger(WebScrapping.class.getName()).log(Level.SEVERE, null, ex);
@@ -112,6 +122,7 @@ public class Ejercicio1 {
         return texto;
     }
 
+    //Funcion para extraer la cantidad de repositorio por cada topico
     public static int procesarCadena(String cadena) {
         return Integer.parseInt(cadena.replace(",", "").substring(9, cadena.indexOf(" ", 9) - 1));
     }
--- java/WebScrapping/src/main/java/py/una/pol/webscrapping/Lenguaje.java
+++ java/WebScrapping/src/main/java/py/una/pol/webscrapping/Lenguaje.java
@@ -4,13 +4,14 @@ package py.una.pol.webscrapping;
  *
  * @author jg2kpy https://github.com/jg2kpy https://juniorgutierrez.com/
  */
-public class Lenguaje {
+public class Lenguaje { // Clase para instancicar por cada lenguaje
 
+    //Atributos de la clase
     String nombre;
     int cantidad;
     float rating;
 
-    public Lenguaje(String nombre, int cantidad, float rating) {
+    public Lenguaje(String nombre, int cantidad, float rating) {//Constructor de la clase
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.rating = rating;
