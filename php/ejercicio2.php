b613477f321da9f733dd0db2ac3e4388fa010b77
Carlos Urdapilleta <carlosurdapilleta@gmail.com>
2022-05-09 12:23:48 -0400

    contar topics y ordenar

--- php/ejercicio2.php
+++ php/ejercicio2.php
@@ -17,6 +17,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_URL, $url);
 $html = curl_exec($ch);
 $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$final = false;
 #parsing
 if ($httpCode == 200) {
 	$topics = [];
@@ -25,6 +26,7 @@ if ($httpCode == 200) {
 	preg_match_all($re, $html, $matches);
 	foreach ($matches as $key1 => $value1) {
 		foreach ($value1 as $key2 => $value2) {
+			if ($final) break;
 			#extraer topics
 			$re = '/href="\/topics\/.*?"/ms';
 			preg_match_all($re, $value2, $topics);
@@ -34,25 +36,30 @@ if ($httpCode == 200) {
 			if (strcmp($fechaActualizacion[0], $hoy[0]) == 0)
 			{
 				print("MISMO MES<br>");
+				contarTopics($topics, $mapaTopics);
 			}
 			elseif (strcmp($fechaActualizacion[0], $hace30Dias[0]) == 0) {
 				print("MES PASADO");
 				$dia1 = (int)$fechaActualizacion[1];
 				$dia2 = (int)$hace30Dias[1];
 				if ($dia1 >= $dia2) {
-					print(" CUMPLE");
+					print(" CUMPLE<br>");
+					contarTopics($topics, $mapaTopics);
 				}
 				else {
-					print(" NO CUMPLE");
+					print(" NO CUMPLE<br>");
+					$final = true;
 				}
-				print("<br>");
 			}
-			else print("NO CUMPLE<br>");
-			// print_r($topics);
-			// print("<br>");
+			else {
+				print("NO CUMPLE<br>");
+				$final = true;
+			}
 			print("<br><br>");
 		}
 	}
+	arsort($mapaTopics);
+	print_r($mapaTopics);
 }
 else {
 	print("ERROR EN ".$lengActual["lenguaje"]." NUMERO ".$httpCode."<br>");
@@ -66,5 +73,15 @@ function obtenerFechaActualizacion($str) {
 	$str = strip_tags($tiempo[0][0]);
 	$str = preg_replace($re, "", $str);
 	return explode(" ", $str);
+}
+
+#se pasa el mapa por referencia y se actualiza para cada repositorio
+function contarTopics($topics, &$map) {
+	foreach ($topics[0] as $topic) {
+		$topic = substr($topic, 14, -1);
+		if (array_key_exists($topic, $map)) $map[$topic]++;
+		else $map[$topic] = 1;
+	}
+	return;
 }
 ?>
